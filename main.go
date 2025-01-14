b44d3209681458275ddd398c98b4eaba623722e5
Owen Schwartz <owen@txv.io>
2025-01-13 22:52:11 -0500

    Allow changing mtu; set default low

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -22,6 +23,7 @@ import (
 var (
 	interfaceName = "wg0"
 	listenAddr    = ":3003"
+	mtu           = 1420
 	lastReadings  = make(map[string]PeerReading)
 	mu            sync.Mutex
 )
@@ -77,6 +79,7 @@ func main() {
 
 	// Define command line flags
 	interfaceNameArg := flag.String("interface", "wg0", "Name of the WireGuard interface")
+	mtuRead := flag.String("mtu", "1280", "MTU of the interface")
 	configFile := flag.String("config", "", "Path to local configuration file")
 	remoteConfigURL := flag.String("remoteConfig", "", "URL to fetch remote configuration")
 	listenAddrArg := flag.String("listen", ":3003", "Address to listen on")
@@ -95,6 +98,11 @@ func main() {
 	}
 	if *listenAddrArg != "" {
 		listenAddr = *listenAddrArg
+	}
+
+	mtu, err = strconv.Atoi(*mtuRead)
+	if err != nil {
+		logger.Fatal("Failed to parse MTU: %v", err)
 	}
 
 	// Validate that only one config option is provided
@@ -288,6 +296,11 @@ func ensureWireguardInterface(wgconfig WgConfig) error {
 	if err != nil {
 		return fmt.Errorf("failed to get interface: %v", err)
 	}
+
+	if err := netlink.LinkSetMTU(link, mtu); err != nil {
+		return fmt.Errorf("failed to set MTU: %v", err)
+	}
+
 	if err := netlink.LinkSetUp(link); err != nil {
 		return fmt.Errorf("failed to bring up interface: %v", err)
 	}
